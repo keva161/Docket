c7b963b4a7092bd51bc443f01d02559358efc793
Kevin Tuck <keva16@gmail.com>
2019-11-21 14:44:39 +0000

    Added login functionality and basic database operations

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.dirname(__file__)
 
 class Config(object):
     SECRET_KEY = 'NOT_SO_SECRET'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- todo.py
+++ todo.py
@@ -1,2 +1,2 @@
-from app import app
-
+from app import app, db
+from app.models import User, Todo
