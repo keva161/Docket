f0639c9bbac4efea3e3aeefb45852ca7969bde54
Kevin Tuck <keva16@gmail.com>
2020-04-01 14:02:36 +0100

    Fixed bug in routes.py

--- app/routes.py
+++ app/routes.py
@@ -20,9 +20,6 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('Docket.login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
